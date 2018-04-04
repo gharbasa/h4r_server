c7d94cc5410f4097d80b6697e95cbbee272bd504
Abed Ali <abedali@engineer.com>
2018-04-04 15:51:25 -0400

    house note

--- app/models/house_note.rb
+++ app/models/house_note.rb
@@ -2,6 +2,7 @@ class HouseNote < ActiveRecord::Base
   attr_accessible :note, :active, 
                   :created_by,:updated_by, :created_at, :updated_at,
                   :house_id, :private
+  alias_attribute :private_note, :private
   belongs_to :house
   
   def deactivate!
