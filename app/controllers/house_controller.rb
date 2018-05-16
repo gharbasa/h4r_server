47441359af03aa2114404899634b281a13fe6843
Abed Ali <abedali@engineer.com>
2018-05-16 19:46:29 -0400

    rename

---
+++ app/controllers/house_controller.rb
@@ -0,0 +1,5 @@
+class HouseController < ApplicationController
+  def index
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -192,7 +192,7 @@ ActiveRecord::Schema.define(version: 20150827030447) do
   
   create_table :payments do |t|   #Payment transactions
     t.integer  :user_house_contract_id, :null => false
-    t.float    :payment,                :null => false
+    t.float    :amount,                :null => false
     t.integer  :payment_status,         :default => 0 #pending, complete, failed
     t.integer  :payment_type,           :default => 0 #initial payment for showing house? or rent payment
     t.integer  :retries_count,          :default => 0 #retries on failed payments, max.retries will be in the application
